e822881b282e5442e8e7e62bb0dbc6226a6138b7
jhonestebanpenagos-maker <jhonestebanpenagos@gmail.com>
2025-11-30 21:46:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -656,7 +656,7 @@ if choice == "Tablero de Mando":
     st.title("TABLERO DE MANDO")
     mostrar_notificaciones()
     
-       df = run_query("ordenes")
+    df = run_query("ordenes")
     
     if not df.empty:
         # Métricas inteligentes
