34841b7c84f1dd3b26ab1224f012d95cb9f0473e
jhonestebanpenagos-maker <jhonestebanpenagos@gmail.com>
2026-06-24 12:00:57 -0500

    Update __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,2 @@
 # Pages package
-from .kanban import render_kanban
-from .lista import render_lista  # o como tengas tus imports actuales
-from .buzon import render_buzon
-from .programacion import render_programacion  # <---- AGREGA ESTA LÍNEA
+
